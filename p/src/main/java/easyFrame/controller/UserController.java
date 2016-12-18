5ae19106481fab9a4ebf9e27ebd68b9fe99d3439
Tiger <1477736949@qq.com>
2016-12-18 21:39:38 +0800

    spring security

--- p/src/main/java/easyFrame/controller/UserController.java
+++ p/src/main/java/easyFrame/controller/UserController.java
@@ -3,8 +3,12 @@ package easyFrame.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import easyFrame.service.ResponseObject;
+
 @Controller
 @RequestMapping("/admin/user")
 public class UserController {
-
+  public ResponseObject  login(){
+	  
+  }
 }
