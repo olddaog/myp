35b8130ec8c42623a8f75f7b7046357c3e8f3ae3
Tiger <1477736949@qq.com>
2016-12-18 16:13:54 +0800

    add model org,role,user

---
+++ p/src/main/java/easyFrame/model/User.java
@@ -0,0 +1,55 @@
+package easyFrame.model;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	@ManyToOne
+	private Org org;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_role")	
+	private Set<Role> role;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Org getOrg() {
+		return org;
+	}
+	public void setOrg(Org org) {
+		this.org = org;
+	}
+	public Set<Role> getRole() {
+		return role;
+	}
+	public void setRole(Set<Role> role) {
+		this.role = role;
+	}
+	
+	
+	
+}
