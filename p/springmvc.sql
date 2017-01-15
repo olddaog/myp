3a9b8f0998a107ea0ded569a2e15c430e1360326
Tiger <1477736949@qq.com>
2017-01-15 21:32:08 +0800

    .

--- p/springmvc.sql
+++ p/springmvc.sql
@@ -1,38 +1,19 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 11121212
-Source Server Version : 50528
+Source Server         : beiken
+Source Server Version : 50550
 Source Host           : localhost:3306
 Source Database       : springmvc
 
 Target Server Type    : MYSQL
-Target Server Version : 50528
+Target Server Version : 50550
 File Encoding         : 65001
 
-Date: 2017-01-05 17:33:36
+Date: 2017-01-15 21:31:13
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `menu_child`
--- ----------------------------
-DROP TABLE IF EXISTS `menu_child`;
-CREATE TABLE `menu_child` (
-  `t_menu_id` bigint(20) NOT NULL,
-  `children_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`t_menu_id`,`children_id`),
-  UNIQUE KEY `children_id` (`children_id`),
-  KEY `FKF9FFAA3CCBC05B97` (`t_menu_id`),
-  KEY `FKF9FFAA3CBF1ADBC2` (`children_id`),
-  CONSTRAINT `FKF9FFAA3CBF1ADBC2` FOREIGN KEY (`children_id`) REFERENCES `t_menu` (`id`),
-  CONSTRAINT `FKF9FFAA3CCBC05B97` FOREIGN KEY (`t_menu_id`) REFERENCES `t_menu` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of menu_child
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for `menu_children`
@@ -57,24 +38,6 @@ INSERT INTO `menu_children` VALUES ('12', '13');
 INSERT INTO `menu_children` VALUES ('11', '14');
 INSERT INTO `menu_children` VALUES ('12', '15');
 INSERT INTO `menu_children` VALUES ('12', '16');
-
--- ----------------------------
--- Table structure for `menu_role`
--- ----------------------------
-DROP TABLE IF EXISTS `menu_role`;
-CREATE TABLE `menu_role` (
-  `t_menu_id` bigint(20) NOT NULL,
-  `roles_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`t_menu_id`,`roles_id`),
-  KEY `FKA4FEA756CBC05B97` (`t_menu_id`),
-  KEY `FKA4FEA756C7B5755B` (`roles_id`),
-  CONSTRAINT `FKA4FEA756C7B5755B` FOREIGN KEY (`roles_id`) REFERENCES `t_role` (`id`),
-  CONSTRAINT `FKA4FEA756CBC05B97` FOREIGN KEY (`t_menu_id`) REFERENCES `t_menu` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of menu_role
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for `org_children`
@@ -114,32 +77,8 @@ CREATE TABLE `role_menu` (
 -- Records of role_menu
 -- ----------------------------
 INSERT INTO `role_menu` VALUES ('1', '10');
+INSERT INTO `role_menu` VALUES ('2', '11');
 INSERT INTO `role_menu` VALUES ('1', '12');
-
--- ----------------------------
--- Table structure for `t_authority`
--- ----------------------------
-DROP TABLE IF EXISTS `t_authority`;
-CREATE TABLE `t_authority` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `role_id` bigint(20) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  `t_user_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKEE482118299CFD02` (`role_id`),
-  KEY `FKEE482118CEC7C0E2` (`user_id`),
-  KEY `FK76D60138299CFD02` (`role_id`),
-  KEY `FK76D601388A59B417` (`t_user_id`),
-  CONSTRAINT `FK76D60138299CFD02` FOREIGN KEY (`role_id`) REFERENCES `t_role` (`id`),
-  CONSTRAINT `FK76D601388A59B417` FOREIGN KEY (`t_user_id`) REFERENCES `t_user` (`id`),
-  CONSTRAINT `FKEE482118299CFD02` FOREIGN KEY (`role_id`) REFERENCES `t_role` (`id`),
-  CONSTRAINT `FKEE482118CEC7C0E2` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of t_authority
--- ----------------------------
-INSERT INTO `t_authority` VALUES ('1', '1', '1', '1');
 
 -- ----------------------------
 -- Table structure for `t_menu`
@@ -195,12 +134,14 @@ CREATE TABLE `t_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_role
 -- ----------------------------
 INSERT INTO `t_role` VALUES ('1', 'ROLE_ADMIN');
+INSERT INTO `t_role` VALUES ('2', 'ROLE_USER');
+INSERT INTO `t_role` VALUES ('3', 'ROLE_SUP');
 
 -- ----------------------------
 -- Table structure for `t_user`
@@ -216,12 +157,13 @@ CREATE TABLE `t_user` (
   PRIMARY KEY (`id`),
   KEY `FKCB63CCB6362F6F32` (`org_id`),
   CONSTRAINT `FKCB63CCB6362F6F32` FOREIGN KEY (`org_id`) REFERENCES `t_org` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('1', '2', null, '123', '123', '');
+INSERT INTO `t_user` VALUES ('1', '赵亮', '2', '$2a$10$hZwA77L.7I7JePsSjfpCNe1nxbTwm5dHjmjMSbwn3i4uuAf5NQzSW', '123', '');
+INSERT INTO `t_user` VALUES ('2', '陈明', '2', '$2a$10$hZwA77L.7I7JePsSjfpCNe1nxbTwm5dHjmjMSbwn3i4uuAf5NQzSW', '321', '');
 
 -- ----------------------------
 -- Table structure for `user_role`
@@ -235,8 +177,10 @@ CREATE TABLE `user_role` (
   KEY `FK143BF46A299CFD02` (`role_id`),
   CONSTRAINT `FK143BF46A299CFD02` FOREIGN KEY (`role_id`) REFERENCES `t_role` (`id`),
   CONSTRAINT `FK143BF46A8A59B417` FOREIGN KEY (`t_user_id`) REFERENCES `t_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_role
 -- ----------------------------
+INSERT INTO `user_role` VALUES ('1', '1');
+INSERT INTO `user_role` VALUES ('2', '2');
